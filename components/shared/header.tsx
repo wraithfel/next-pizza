5b4dfd672948bd79f64e23ef9be633c52d64ead4
Nikita Lebedev <l3bedev.n@yandex.ru>
2025-05-16 23:29:03 +0300

    hotfix scroll

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -9,41 +9,40 @@ interface Props{
     className?: string;
 }
 
-export const Header: React.FC<Props> = ({ className }) => {
-  return (
-    <header className={cn('border-b overflow-x-hidden', className)}>
-      <Container className="flex items-center justify-between py-8">
-        {/* Левая часть */}
-        <div className="flex items-center gap-3">
-          <Image src="/logo.png" alt="Logo" width={35} height={35} />
-          <div>
-            <h1 className="text-2xl uppercase font-black">Next Pizza</h1>
-            <p className="text-sm text-gray-400 leading-3">
-              вкусней уже некуда
-            </p>
-          </div>
-        </div>
-
-        {/* Правая часть */}
-        <div className="flex items-center gap-3 flex-wrap">
+export const Header: React.FC<Props> = ({className}) => {
+    return (
+        <header className = {cn('border border-b', className)}>
+        <Container className='flex items-center justify-between py-8'>
+            {/* Левая часть */}
+            <div>
+                <Image src="/logo.png" alt="Logo" width={35} height={35} />
+                <div>
+                    <h1 className="text-2xl uppercase font-black">Next Pizza </h1>
+                    <p className="text-sm text-gray-400 leading-3">вкусней уже некуда</p>
+                </div>
+            </div>
+            {/* Правая часть */}
+            <div className='flex flex-start gap-3 pr-5'>
+            <div className="flex items-center gap-3">
           <Button variant="outline" className="flex items-center gap-1">
             <User size={16} />
-            <span className="hidden sm:inline">Войти</span>
-          </Button>
-
-          <Button className="group relative flex items-center gap-3">
-            <b>520 ₽</b>
-            <span className="h-full w-px bg-white/30"></span>
-            <div className="flex items-center gap-1 transition-opacity duration-300 group-hover:opacity-0">
-              <ShoppingCart className="h-4 w-4" strokeWidth={2} />
-              <b>3</b>
+            Войти
+            </Button>
             </div>
-            <ArrowRight
-              className="absolute right-3 w-5 transition-all duration-300 -translate-x-2 opacity-0 group-hover:translate-x-0 group-hover:opacity-100"
-            />
-          </Button>
+            <div>
+             <Button className="group relative overflow-x-hidden">
+              <b>520 ₽</b>
+              <span className="h-full w-[1px] bg-white/30 mx-3" />
+              <div className="flex items-center gap-1 transition duration-300 group-hover:opacity-0">
+                <ShoppingCart className="h-4 w-4 relative" strokeWidth={2} />
+                <b>3</b>
+              </div>
+              <ArrowRight className="w-5 absolute right-5 transition duration-300 -translate-x-2 opacity-0 group-hover:opacity-100 group-hover:translate-x-0" />
+            </Button>
         </div>
-      </Container>
-    </header>
-  );
-};
+        </div>
+         </Container>
+        </header>
+
+    )
+}
