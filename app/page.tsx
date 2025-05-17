9b1feb5b766bc1a2eb575d452f6e81264f7bcbf5
Nikita Lebedev <l3bedev.n@yandex.ru>
2025-05-17 17:40:31 +0300

    add filters

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,32 @@
-import { Container } from "@/components/shared";
+import { Container, SortPopup } from "@/components/shared";
 import { Title } from "@/components/shared";
+import { TopBar } from "@/components/shared";
+import { Filters } from "@/components/shared";
 
 
 export default function Home() {
   return(
-  <Container className="mt-10">
-    <Title size="lg" text="Все пиццы" className="font-extrabold" />
-  </Container>)
+    <>
+    <Container className="mt-10">
+        <Title size="lg" text='Все пиццы' className="font-extrabold"/>
+    </Container>
+    <TopBar/>
+
+    <Container className="pb-14">
+      <div className="flex gap-[60px]">
+
+        <div className="w-[250px]">
+          <Filters />
+        </div>
+
+        <div className="gap-1">
+          <div className="flex flex-col gap-16">
+            Список товаров
+          </div>
+        </div>
+
+      </div>
+    </Container>
+    </>
+  )
 }
