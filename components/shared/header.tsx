55f2918c77052770b02441a107ebac8c01733a95
Nikita Lebedev <l3bedev.n@yandex.ru>
2025-05-21 19:32:49 +0300

    add products and ingredients api + do search layout

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -4,6 +4,8 @@ import { Container } from '../shared';
 import Image from 'next/image';
 import { Button } from '../ui';
 import { ArrowRight, ShoppingCart, User } from 'lucide-react';
+import Link from 'next/link';
+import { SearchInput } from './search-input';
 
 interface Props{
     className?: string;
@@ -14,12 +16,18 @@ export const Header: React.FC<Props> = ({className}) => {
         <header className = {cn('border border-b', className)}>
         <Container className="flex flex-col sm:flex-row sm:items-center sm:justify-between gap-4 py-4 sm:py-6">
             {/* Левая часть */}
+            <Link href='/'>
             <div>
                 <Image src="/logo.png" alt="Logo" width={35} height={35} />
                 <div>
                     <h1 className="text-2xl uppercase font-black">Next Pizza </h1>
                     <p className="text-sm text-gray-400 leading-3">вкусней уже некуда</p>
                 </div>
+            </div>
+            </Link>
+
+            <div className='mx-10 flex-1'>
+                <SearchInput />
             </div>
             {/* Правая часть */}
             <div className='flex flex-start gap-3 pr-5'>
