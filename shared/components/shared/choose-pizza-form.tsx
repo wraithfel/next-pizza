835ea8f2c3ccf759ab04f7330b44dc99bad2293e
Nikita Lebedev <l3bedev.n@yandex.ru>
2025-05-28 19:59:00 +0300

    customize modal

--- shared/components/shared/choose-pizza-form.tsx
+++ shared/components/shared/choose-pizza-form.tsx
@@ -3,12 +3,15 @@ import React from 'react'
 import { ProductImage } from './product-image'
 import { Title } from './title'
 import { Button } from '../ui'
+import { GroupVariants } from './group-variants'
+import { PizzaSize, pizzaSizes, PizzaType, pizzaTypes } from '@/shared/constants/pizza'
+import { Ingredient } from '@prisma/client'
 
 interface Props {
   imageUrl: string
   name: string
   items?: any[]
-  ingredients: any[]
+  ingredients: Ingredient[]
   onClickAdd?: VoidFunction
   className?: string
 }
@@ -21,13 +24,15 @@ export const ChoosePizzaForm: React.FC<Props> = ({
   onClickAdd,
   className,
 }) => {
+  const [size, setSize] = React.useState<PizzaSize>(20);
+  const [type, setType] = React.useState<PizzaType>(1);
+
   const textDetails = '30 см, традиционное тесто'
   const totalPrice = 350
-  const size = 30
 
   return (
-    <div className={cn('flex flex-row gap-30 items-center', className)}>
-      <div className="flex-shrink-0">
+    <div className={cn('flex flex-row gap-30', className)}>
+      <div className="flex-shrink-0 w-[500px] h-[500px] flex justify-center items-center overflow-visible">
         <ProductImage imageUrl={imageUrl} size={size}/>
       </div>
 
@@ -41,7 +46,14 @@ export const ChoosePizzaForm: React.FC<Props> = ({
         <div>
           <Title text={name} size="md" className="font-extrabold mb-3" />
           <p className="text-gray-400 mb-6">{textDetails}</p>
-          {/* TODO: здесь появятся варианты размера/теста */}
+          <div className='flex flex-col gap-4 mt-5'>
+              <GroupVariants items={pizzaSizes} selectedValue={String(size)} onClick={value => setSize(Number(value) as PizzaSize)} />
+              <GroupVariants items={pizzaTypes} selectedValue={String(type)} onClick={value => setType(Number(value) as PizzaType)} />
+          </div>
+
+          <div className='grid grid-cols-3 gap-3'>
+              
+          </div>
         </div>
 
         <Button className="h-[55px] px-10 text-base rounded-[18px] w-full">
--- shared/components/shared/modals/choose-product-modal.tsx
+++ shared/components/shared/modals/choose-product-modal.tsx
@@ -32,7 +32,7 @@ export const ChooseProductModal: React.FC<Props> = ({ product, className }) => {
         {isPizzaForm ? (
           <ChoosePizzaForm imageUrl={product.imageUrl}
           name={product.name}
-          ingredients={[]} 
+          ingredients={product.ingredients} 
           />
         ): 
         <ChooseProductForm
