09bd28e3a69a89cff217bdbf8b2a60231f8a01c5
Nikita Lebedev <l3bedev.n@yandex.ru>
2025-06-01 17:24:15 +0300

    hotfix auth

--- shared/services/auth.ts
+++ shared/services/auth.ts
@@ -1,6 +1,6 @@
 import { axiosInstance } from './instance';
 
-export const me       = () => axiosInstance.get('/auth/me').then(r => r.data);
-export const login    = (d:{email:string,password:string})  => axiosInstance.post('/auth/login', d).then(r=>r.data);
-export const register = (d:{fullName:string,email:string,password:string}) => axiosInstance.post('/auth/register', d).then(r=>r.data);
-export const logout   = () => axiosInstance.post('/auth/logout').then(()=>true);
+export const me       = () => axiosInstance.get('auth/me').then(r => r.data);
+export const login    = (d:{email:string,password:string})  => axiosInstance.post('auth/login', d).then(r=>r.data);
+export const register = (d:{fullName:string,email:string,password:string}) => axiosInstance.post('auth/register', d).then(r=>r.data);
+export const logout   = () => axiosInstance.post('auth/logout').then(()=>true);
