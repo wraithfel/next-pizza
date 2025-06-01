4c0040360d1f512569737d879c6277ad9492e1a8
Nikita Lebedev <l3bedev.n@yandex.ru>
2025-06-02 00:45:04 +0300

    fix types

--- app/(root)/@modal/(.)product/[id]/page.tsx
+++ app/(root)/@modal/(.)product/[id]/page.tsx
@@ -1,27 +1,21 @@
-import { ChooseProductModal } from "@/shared/components/shared/modals";
-import { prisma } from "@/prisma/prisma-client";
-import { notFound } from "next/navigation";
+import { ChooseProductModal } from '@/shared/components/shared/modals'
+import { prisma } from '@/prisma/prisma-client'
+import { notFound } from 'next/navigation'
 
-export default async function ProductModalPage(props: { params: Promise<{ id: string}>}) {
-    const params = await props.params;
-    
-    const {
-        id
-    } = params;
+export default async function ProductModalPage({
+  params,
+}: {
+  params: { id: string }
+}) {
+  const product = await prisma.product.findFirst({
+    where: { id: Number(params.id) },
+    include: {
+      items: true,
+      ingredients: true,
+    },
+  })
 
-    const product = await prisma.product.findFirst({
-        where: {
-            id: Number(id),
-        },
-        include: {
-            ingredients: true,
-            items: true
-        },
-    })
+  if (!product) return notFound()
 
-    if (!product) {
-        return notFound();
-    }
-                    
-    return <ChooseProductModal product={product} />
+  return <ChooseProductModal product={product} />
 }
--- app/(root)/product/[id]/page.tsx
+++ app/(root)/product/[id]/page.tsx
@@ -1,26 +1,26 @@
-import { prisma } from '@/prisma/prisma-client';
-import { notFound } from 'next/navigation';
-import { ProductScreen } from '@/shared/components/shared';
+import { prisma } from '@/prisma/prisma-client'
+import { notFound } from 'next/navigation'
+import { ProductScreen } from '@/shared/components/shared'
 
-interface Props {
-  params: { id: string };
-}
-
-export default async function ProductPage({ params }: Props) {
+export default async function ProductPage({
+  params,
+}: {
+  params: { id: string }
+}) {
   const product = await prisma.product.findFirst({
     where: { id: Number(params.id) },
     include: {
-      items:       true,
+      items: true,
       ingredients: true,
-      category:    true,
+      category: true,
     },
-  });
+  })
 
-  if (!product) return notFound();
+  if (!product) return notFound()
 
   return (
     <div className="min-h-screen bg-[#F7F6F5] py-10">
       <ProductScreen product={product} />
     </div>
-  );
+  )
 }
